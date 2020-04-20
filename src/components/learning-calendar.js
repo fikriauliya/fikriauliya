acc68c1bf83954798dcfc52c10047f02a707deb6
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-04-20 23:49:46 +0700

    Make heatmap responsive

--- src/components/learning-calendar.js
+++ src/components/learning-calendar.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import CalendarHeatmap from 'react-calendar-heatmap'
+import Grid from '@material-ui/core/Grid';
 
 const LearningCalendar = () => {
   const data = useStaticQuery(graphql`
@@ -27,19 +28,23 @@ const LearningCalendar = () => {
   const endDate = new Date(heatmap[heatmap.length - 1].date)
   const startDate = new Date(endDate)
   startDate.setMonth(startDate.getMonth() - 6)
-  return <CalendarHeatmap
-    startDate={startDate}
-    endDate={endDate}
-    values={heatmap}
-    showWeekdayLabels={true}
-    gutterSize={2}
-    classForValue={(value) => {
-      if (!value) {
-        return 'color-empty';
-      }
-      return `color-github-${value.count}`;
-    }}
-  />
+  return <Grid container justify="center">
+    <Grid item xs={12} sm={6} md={4}>
+      <CalendarHeatmap
+        startDate={startDate}
+        endDate={endDate}
+        values={heatmap}
+        showWeekdayLabels={true}
+        gutterSize={2}
+        classForValue={(value) => {
+          if (!value) {
+            return 'color-empty';
+          }
+          return `color-github-${value.count}`;
+        }}
+      />
+    </Grid>
+  </Grid>
 }
 
 export default LearningCalendar
